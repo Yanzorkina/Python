54c9ce4cd4c810f9734a8df95981ea0ecb9a0e40
Alina <alinayanzorkina@gmail.com>
2021-12-01 16:47:38 +0300

    task 3_1

---
+++ 3_1.py
@@ -0,0 +1,21 @@
+
+nums = {
+    'zero': 'ноль',
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+
+
+def num_translate():
+    return nums.get(input("Введите число на английском языке: "))
+
+print(num_translate())
+
