75915bae69964b0bf12808576a783221a07732b7
Alina <alinayanzorkina@gmail.com>
2021-12-08 17:35:09 +0300

    5_2

--- 5_2.py
+++ 5_2.py
@@ -1,2 +1,4 @@
 
-nums_gen = [print(num) for num in range(1, (int(input("Введите число n: ")) + 1)) if num % 2 != 0]
+nums_gen = (print(num) for num in range(1, (int(input("Введите число n: ")) + 1)) if num % 2 != 0)
+
+print(type(nums_gen))
